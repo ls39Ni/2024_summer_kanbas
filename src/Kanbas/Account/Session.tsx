2159c82617fa58610eedbddcc6742229d6e72175
NIKITA LIANG <nikitaliang@NIKITAs-MBP.lan>
2024-07-29 20:06:14 -0700

    test

--- src/Kanbas/Account/Session.tsx
+++ src/Kanbas/Account/Session.tsx
@@ -1,11 +1,13 @@
 import * as client from "./client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { setCurrentUser } from "./reducer";
 import { useDispatch } from "react-redux";
+
 export default function Session({ children }: { children: any }) {
   const [pending, setPending] = useState(true);
   const dispatch = useDispatch();
-  const fetchProfile = async () => {
+
+  const fetchProfile = useCallback(async () => {
     try {
       const currentUser = await client.profile();
       dispatch(setCurrentUser(currentUser));
@@ -13,10 +15,12 @@ export default function Session({ children }: { children: any }) {
       console.error(err);
     }
     setPending(false);
-  };
+  }, [dispatch]);
+
   useEffect(() => {
     fetchProfile();
-  }, []);
+  }, [fetchProfile]);
+
   if (!pending) {
     return children;
   }
